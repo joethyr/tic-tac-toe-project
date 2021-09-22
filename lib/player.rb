800d215b913d8a7d45ea717897dd36c854a1872a
joethyr <joethayr@gmail.com>
2021-09-21 21:54:06 -0600

    Added initilize method on all class files

---
+++ lib/player.rb
@@ -0,0 +1,8 @@
+class Player
+  attr_accessor :name
+
+  def initialize(name, symbol)
+    @name = name
+    @symbol = symbol
+  end
+end
--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,8 +1,36 @@
 # frozen_string_literal: true
 
-require_relative "tic_tac_toe/version"
+require_relative 'player'
+require_relative 'board'
 
-module TicTacToe
-  class Error < StandardError; end
-  # Your code goes here...
+class TicTacToe
+  attr_accessor :player1, :player2
+
+  def initialize(options)
+    options = defaults.merge(options)
+
+    @board = Board.new
+    @player1 = options[:player1]
+    @player2 = options[:player2]
+  end
+
+  def defaults
+    { player1: Player.new('Player 1', 'X'),
+      player2: Player.new('Player 2', 'O') }
+  end
 end
+
+joe = TicTacToe.new(player1: Player.new('Joe', 'J'), player2: Player.new('Bob', 'B'))
+
+# Set up the game initially [tictactoe]
+#   Create a game board [board]
+#   Create a couple players [player]
+
+# Start the game loop [tictactoe]
+#   Display the board [board]
+#   Ask for and validate the current player's move [player]
+#   If the game should end [tictactoe]
+#     Display the proper victory / draw prompt
+#     Stop looping
+#   Else
+#     Switch to the next player and keep looping [tictactoe]
