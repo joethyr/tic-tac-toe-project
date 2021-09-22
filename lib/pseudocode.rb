0f6ec1171c5489b4162213b706632aa6035d2b21
joethyr <joethayr@gmail.com>
2021-09-21 20:09:18 -0600

    added high level pseudocode

---
+++ lib/pseudocode.rb
@@ -0,0 +1,16 @@
+# 2. Psuedocode the High level Approach
+
+# Set up the game initially
+#   Create a game board
+#   Create a couple players
+
+# Start the game loop
+#   Display the board
+#   Ask for and validate the current player's move
+#   If the game should end
+#     Display the proper victory / draw prompt
+#     Stop looping
+#   Else
+#     Switch to the next player and keep looping
+
+# A high level pseudocoding will cover the major actions
